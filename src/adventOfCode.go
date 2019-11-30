9e0163f41443094bca23ff6b449853a8cdd87b89
Arne Engelbrecht <ArneE@discovery.co.za>
2019-11-30 21:56:40 +0200

    Added a menuing system

---
+++ src/adventOfCode.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	directory := readFilesFromDirectory()
+	for {
+		fmt.Println("Which day do you want to run?")
+		for i, f := range directory {
+			fmt.Printf("%v: %v\r\n", i+1, f)
+		}
+		fmt.Print("->")
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		i, err := strconv.ParseInt(text, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		handleInput(directory[i-1])
+	}
+}
+
+func readFilesFromDirectory() []string {
+	files, err := ioutil.ReadDir("./src/")
+	challenges := []string{}
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		if f.Name() != "adventOfCode.go" && !strings.Contains(f.Name(), "test") {
+			challenges = append(challenges, f.Name())
+		}
+	}
+	challenges = append(challenges, "Exit")
+	return challenges
+}
+
+func handleInput(input string) {
+	switch input {
+	case "Exit":
+		os.Exit(10)
+		break
+	default:
+		fmt.Println("wat")
+	}
+}
