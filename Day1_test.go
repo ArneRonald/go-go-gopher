4a0c789bc262b133e8a4718a83aee9cea1842f2a
Arne Engelbrecht <ArneE@discovery.co.za>
2019-11-30 15:03:28 +0200

    Completed Day 1 challenge

---
+++ Day1_test.go
@@ -0,0 +1,60 @@
+package main
+
+import "testing"
+
+func TestGetFloorScenarioOne(t *testing.T) {
+	firstResult := getFloor("(())")
+	if firstResult != 0 {
+		t.Errorf("Incorrect, Result was %v", firstResult)
+	}
+
+	secondResult := getFloor("()()")
+	if secondResult != 0 {
+		t.Errorf("Incorrect, Result was %v", secondResult)
+	}
+}
+
+func TestGetFloorScenarioTwo(t *testing.T) {
+	firstResult := getFloor("(((")
+	if firstResult != 3 {
+		t.Errorf("Incorrect, Result was %v", firstResult)
+	}
+
+	secondResult := getFloor("(()(()(")
+	if secondResult != 3 {
+		t.Errorf("Incorrect, Result was %v", secondResult)
+	}
+}
+
+func TestGetFloorScenarioThree(t *testing.T) {
+	firstResult := getFloor("))(((((")
+	if firstResult != 3 {
+		t.Errorf("Incorrect, Result was %v", firstResult)
+	}
+}
+
+func TestGetFloorScenarioFour(t *testing.T) {
+	firstResult := getFloor("())")
+	if firstResult != -1 {
+		t.Errorf("Incorrect, Result was %v", firstResult)
+	}
+
+	secondResult := getFloor("))(")
+	if secondResult != -1 {
+		t.Errorf("Incorrect, Result was %v", secondResult)
+	}
+}
+
+func TestGetFloorScenarioFive(t *testing.T) {
+	firstResult := getFloor(")))")
+	if firstResult != -3 {
+		t.Errorf("Incorrect, Result was %v", firstResult)
+	}
+
+	secondResult := getFloor(")())())")
+	if secondResult != -3 {
+		t.Errorf("Incorrect, Result was %v", secondResult)
+	}
+}
+
+
